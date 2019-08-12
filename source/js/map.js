a0db21a1fc14436d5344aefdd930118742f05ba0
Olga Bazhenova <olgabazhenova07@yandex.ru>
2019-08-12 17:55:15 +0300

    Уточняет и дополняет скрипт для Google-карты map.js

--- source/js/map.js
+++ source/js/map.js
@@ -1,22 +1,49 @@
 'use strict';
 
 function initMap() {
-  var myLatLng = {lat: 59.938549, lng: 30.322993};
-  var map = new google.maps.Map(document.querySelector('.contacts__map'), {
-    zoom: 17,
-    center: myLatLng,
-    scrollwheel: false
-  });
+  var ZOOM = 17;
+  var TITLE = 'Мишка';
 
-  var pin = {
-    url: 'img/icon-map-pin.svg',
-    scaledSize: new google.maps.Size(67, 100),
+  var LAT_LNG = {
+    lat: 59.938549,
+    lng: 30.322993
   };
 
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: 'Мишка',
-    icon: pin
-  });
+  var MAP_OPTIONS = {
+    zoom: ZOOM,
+    center: LAT_LNG,
+    scrollwheel: false
+  };
+
+  var PIN = {
+    url: 'img/icon-map-pin.svg',
+    width: 67,
+    height: 100,
+    originX: 0,
+    originY: 0,
+    anchorX: 33.5,
+    anchorY: 100
+  }
+
+  var mapArea = document.querySelector('.map__interactive');
+
+  if (mapArea !== null) {
+    mapArea.classList.remove('map__interactive--hidden');
+    var map = new google.maps.Map(mapArea, MAP_OPTIONS);
+
+    var pin = {
+      url: PIN.url,
+      size: new google.maps.Size(PIN.width, PIN.height),
+      origin: new google.maps.Point(PIN.originX, PIN.originY),
+      anchor: new google.maps.Point(PIN.anchorX, PIN.anchorY),
+      scaledSize: new google.maps.Size(PIN.width, PIN.height)
+    };
+
+    var marker = new google.maps.Marker({
+      position: LAT_LNG,
+      map: map,
+      title: TITLE,
+      icon: pin
+    });
+  }
 }
